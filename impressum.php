6464c1ce30e2f3b1a837f067e89138de8891dd2c
Jörn Schlingensiepen <schlingensiepen@i3cm.de>
2019-12-11 20:05:50 +0100

    issue #1: impressum.php added and linked

---
+++ impressum.php
@@ -0,0 +1,15 @@
+<?php include("inc/header.inc") ?>
+    <span>
+    <h2>Impressum</h2>
+
+    Tomaten und Kartoffeln<br>
+    Gemüsehof Githuber<br>
+    Ludwig und Marianne Githuber<br>
+    Am Wendacker 4<br>
+    25464 Hintertupfingen<br>
+    <a href="mailto:githuber@tomatenundkartoffeln.de">
+        mailto:githuber@tomatenundkartoffeln.de
+    </a>
+</span>
+
+<?php include("inc/footer.inc") ?>
